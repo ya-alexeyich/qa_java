fed40d6eff1b28ec25e4d353ac07a87fe9e33d22
Alexey Chugunov <lexwinter7749@gmail.com>
2024-12-18 22:58:36 +0400

    First commit

---
+++ src/main/java/com/example/LionAlex.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import java.util.List;
+
+public class LionAlex extends Lion{
+    public static final  String GENDER_ALEX = "Самец";
+    private final Feline feline;
+
+    public LionAlex(Feline feline) throws Exception {
+        super(GENDER_ALEX, feline);
+        this.feline = feline;
+    }
+
+    @Override
+    public int getKittens() {
+        return feline.getKittens(0);
+    }
+
+    public List<String> getFriends() {
+        return List.of("Марти", "Глория", "Мелман");
+    }
+
+    public String getPlaceOfLiving() {
+        return "Нью-Йоркский зоопарк";
+    }
+
+}
